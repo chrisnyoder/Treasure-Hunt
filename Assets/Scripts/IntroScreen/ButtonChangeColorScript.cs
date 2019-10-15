be3a6d673b5b8bd3f278a68144733fc0e2e81eaa
chrisnyoder <chrisnyoder@gmail.com>
2019-10-15 14:26:30 -0700

    added info screen

---
+++ Assets/Scripts/IntroScreen/ButtonChangeColorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ButtonChangeColorScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
+{
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("on pointer down called");
+        GetComponentInChildren<Text>().color = new Color32(255, 239, 210, 130);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        print("on pointer up called");
+        GetComponentInChildren<Text>().color = new Color32(255, 239, 210, 255);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("on pointer exit called");
+        GetComponentInChildren<Text>().color = new Color32(255, 239, 210, 255);
+    }
+
+}
---
+++ Assets/Scripts/IntroScreen/InfoScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
+
+public class InfoScript : MonoBehaviour
+{
+
+    public Canvas InfoPopUp;
+
+    public void bringUpInfoPopUp()
+    {
+        var animator = InfoPopUp.GetComponent<Animator>();
+        animator.Play("StoreInfoPopUpAnimation");
+    }
+
+    public void closePopUp()
+    {
+        var animator = GetComponent<Animator>();
+        animator.Play("StoreInfoPopUpAnimationReverse");
+    }
+
+    public void contactFriendlyPixel()
+    {
+        string email = "contact@friendlypixel.app";
+        string subject = MyEscapeURL("Subject");
+        string body = MyEscapeURL("");
+
+        print("mailto:" + email + "?subject=" + subject + "%body=" + body);
+
+        Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+    }
+
+    private string MyEscapeURL(string URL)
+    {
+        return UnityWebRequest.EscapeURL(URL).Replace("+", "%20");
+    }
+
+    public void bringUpPrivacyPolicy()
+    {
+        Application.OpenURL("https://friendlypixel.app/treasure_hunt_privacy_policy.html");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
