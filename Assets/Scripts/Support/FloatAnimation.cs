4b1e6e47fa794a3c48d935c9acf5e85f6f0bb2d6
chrisnyoder <chrisnyoder@gmail.com>
2019-10-28 16:05:40 -0700

    more tween animations

--- Assets/Scripts/Support/FloatAnimation.cs
+++ Assets/Scripts/Support/FloatAnimation.cs
@@ -5,7 +5,11 @@ using DG.Tweening;
 
 public class FloatAnimation : MonoBehaviour
 {
-    public float radius = 8;
+    public float verticalDistance = 8;
+    public float time = 3; 
+
+//  public float radius = 8;
+    
     RectTransform rectTransform;
     Vector2 initialPosition; 
 
@@ -19,24 +23,32 @@ public class FloatAnimation : MonoBehaviour
     {
         yield return new WaitForSeconds(.1f);
         initialPosition = rectTransform.anchoredPosition;
-        getRandomPoint();   
+        playAnimation();
     }
 
-    void getRandomPoint()
+    void playAnimation()
     {
-        float a = Random.Range(0f, 1f) * 2f * 3.14f;
-        float r = radius * Mathf.Sqrt(Random.Range(0f, 1f));
-
-        int x = (int)(initialPosition.x - r * Mathf.Cos(a));
-        float y = (int)(initialPosition.y - r * Mathf.Sin((float)r));
-
-        moveToPoint(new Vector2(x, y));
+        var anim = rectTransform.DOAnchorPosY(rectTransform.anchoredPosition.y + verticalDistance, time, false);
+        anim.SetEase(Ease.Linear);
+        anim.SetLoops(-1, LoopType.Yoyo);
+        anim.Play();
     }
 
-    void moveToPoint(Vector2 newPoint)
-    {
-        var animation = rectTransform.DOAnchorPos(newPoint, 2f, false).SetEase(Ease.Linear);   
-        animation.Play();
-        animation.OnComplete(getRandomPoint);
-    }
+    // void getRandomPoint()
+    // {
+    //     float a = Random.Range(0f, 1f) * 2f * 3.14f;
+    //     float r = radius * Mathf.Sqrt(Random.Range(0f, 1f));
+
+    //     int x = (int)(initialPosition.x - r * Mathf.Cos(a));
+    //     float y = (int)(initialPosition.y - r * Mathf.Sin((float)r));
+
+    //     moveToPoint(new Vector2(x, y));
+    // }
+
+    // void moveToPoint(Vector2 newPoint)
+    // {
+    //     var animation = rectTransform.DOAnchorPos(newPoint, 2f, false).SetEase(Ease.Linear);   
+    //     animation.Play();
+    //     animation.OnComplete(getRandomPoint);
+    // }
 }
