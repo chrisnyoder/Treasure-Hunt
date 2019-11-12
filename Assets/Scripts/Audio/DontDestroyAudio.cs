39616e5b69491c6ef9ab3d2a994cc5f6fb151737
Retsnad <danielbyoder@gmail.com>
2019-11-12 13:33:58 +0900

    Added background muisc and a way to toggle it on and off in Settings

---
+++ Assets/Scripts/Audio/DontDestroyAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAudio : MonoBehaviour
+{
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+
+
+
+
+
+}
