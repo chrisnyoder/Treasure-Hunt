22a7dd26dae80cc000a597c523be1f6bcd976303
Retsnad <danielbyoder@gmail.com>
2019-12-06 20:28:11 +0900

    Added Audio to a ton of buttons

---
+++ Assets/Scripts/Support/ButtonPressWithSfx.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class ButtonPressWithSfx : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+
+    RectTransform rectTransform;
+
+    Vector3 pushedScale;
+    Vector3 initialScale;
+
+    // Use this for initialization
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        initialScale = rectTransform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        rectTransform = GetComponent<RectTransform>();
+        pushedScale = initialScale * 0.85f;
+        transform.DOScale(pushedScale, 1.0f).SetEase(Ease.OutExpo);
+        GlobalAudioScript.Instance.playSfxSound2("click2");
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        var scaleBack = rectTransform.DOScale(initialScale, 1.0f).SetEase(Ease.OutBounce);
+        scaleBack.Play();
+    }
+}
