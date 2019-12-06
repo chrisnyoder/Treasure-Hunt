4636d4b5c7fdb092607093693e9fb934b7914d80
Retsnad <danielbyoder@gmail.com>
2019-12-06 19:24:47 +0900

    ButtonPushed script fixed

--- Assets/Scripts/Support/ButtonPress.cs
+++ Assets/Scripts/Support/ButtonPress.cs
@@ -5,39 +5,12 @@ using UnityEngine;
 using DG.Tweening;
 
 
-public class ButtonPress : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler
+public class ButtonPress : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
-    public void OnDrag(PointerEventData pointerEventData)
-    {
-        rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
-        rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
-        transform.Rotate(Vector3.forward, -rotX);
-        transform.Rotate(Vector3.right, rotY);
 
-        angleX += Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
-        angleX = Mathf.Clamp(angleX, -5.0f, 5.0f);
-        transform.rotation = Quaternion.Euler(angleY, 0.0f, -angleX);
-
-        angleY += Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
-        angleY = Mathf.Clamp(angleY, -10.0f, 10.0f);
-        transform.rotation = Quaternion.Euler(angleY, 0.0f, -angleX);
-
-    }
-
-    public void OnEndDrag(PointerEventData pointerEventData)
-    {
-        var rotateBack = rectTransform.DORotate(initialRotation, 0.5f, RotateMode.Fast);
-        rotateBack.Play();
-    }
-
-    float angleX = -0.0f;
-    float angleY = 0.0f;
-    float rotSpeed = 10.0f;
-    float rotX;
-    float rotY;
     RectTransform rectTransform;
-    Vector3 initialRotation;
+   
     Vector3 pushedScale;
     Vector3 initialScale;
 
@@ -45,7 +18,7 @@ public class ButtonPress : MonoBehaviour, IDragHandler, IEndDragHandler, IPointe
     void Start()
     {
         rectTransform = GetComponent<RectTransform>();
-        initialRotation = new Vector3(0, 0, 0);
+       
         initialScale = rectTransform.localScale;
     }
 
@@ -58,7 +31,7 @@ public class ButtonPress : MonoBehaviour, IDragHandler, IEndDragHandler, IPointe
     public void OnPointerDown(PointerEventData eventData)
     {
         rectTransform = GetComponent<RectTransform>();
-        pushedScale = initialScale * 0.8f;
+        pushedScale = initialScale * 0.85f;
         transform.DOScale(pushedScale, 1.0f).SetEase(Ease.OutExpo);
     }
 
