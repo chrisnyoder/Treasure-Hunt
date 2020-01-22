0389136ce8503a4976a8e18959948f144d5275d4
Retsnad <danielbyoder@gmail.com>
2020-01-22 21:57:22 +0900

    Added Restart Button to Menu
    Still need to have a warning popup but otherwise it seems to be working as intended

--- Assets/Scripts/Animation/RotateAnimation.cs
+++ Assets/Scripts/Animation/RotateAnimation.cs
@@ -14,14 +14,14 @@ public class RotateAnimation : MonoBehaviour, IPointerDownHandler
   
         if (ButtonState == true)
         {
-            var rotate = transform.DORotate(new Vector3(0, 0, 180), 0.5f, RotateMode.Fast);
+            var rotate = transform.DORotate(new Vector3(0, 0, 180), 0.7f, RotateMode.Fast);
             ButtonState = false;
             print("Buttonstate is false");
         }
 
         else 
         {
-            var rotateBack = transform.DORotate(new Vector3(0, 0, 360), 0.5f, RotateMode.Fast);
+            var rotateBack = transform.DORotate(new Vector3(0, 0, 360), 0.7f, RotateMode.Fast);
             ButtonState = true;
             print("ButtonState is true");
         }
---
+++ Assets/Scripts/Audio/RotateAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateAudio : MonoBehaviour, IPointerDownHandler
+
+{
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        GlobalAudioScript.Instance.playSfxSound("pickup");
+    }
+
+
+}
