27275926f4f9ba4f25ca7478a3601b1ea172c097
Retsnad <danielbyoder@gmail.com>
2020-01-23 13:32:16 +0900

    EoG Game Polish, Crew Board Menu Functionality
    Crew Board Restart not working. Not sure why

---
+++ Assets/Scripts/Audio/CoinDropAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CoinDropAudio : MonoBehaviour, IPointerUpHandler
+
+{
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        GlobalAudioScript.Instance.playSfxSound("coin_drop");
+    }
+
+
+   
+
+
+
+}
