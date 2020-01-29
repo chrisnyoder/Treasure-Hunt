04f1d39c8522f4dd831a4b2416b70e3691458aea
Retsnad <danielbyoder@gmail.com>
2020-01-29 16:59:16 +0900

    Click ouside Menu to close it
    Currently works if you click the background. If you click on a Tile is won't close. Need to add sfx

---
+++ Assets/Scripts/Support/IfClickToExitMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.UI;
+public class IfClickToExitMenu : MonoBehaviour
+{
+
+
+    public MainboardMenuHandler mainboardMenuHandlerRef;
+    public RotateAnimation rotateAnimationRef;
+
+    void Start()
+    {
+        rotateAnimationRef = GameObject.Find("MenuPulloutToggle").GetComponent<RotateAnimation>();
+        mainboardMenuHandlerRef = GameObject.Find("MenuPulloutParent").GetComponent<MainboardMenuHandler>();
+    }
+
+    public void ExitIfYouCan()
+    {
+        if (rotateAnimationRef.MenuIsClosed != true)
+        {
+            rotateAnimationRef.rotateMenu();
+            mainboardMenuHandlerRef.toggleMenu();
+            
+            print("I'm rotating the arrow back to its starting position");
+        }
+
+        else
+        {
+            print("I did nothing cos the menu wasn't out");
+        }
+
+    }
+
+    // Start is called before the first frame update
+
+
+
+
+}
