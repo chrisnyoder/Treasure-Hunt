1ff6cf477e8ba6f0f675b8e396509adce2564c33
Retsnad <danielbyoder@gmail.com>
2020-02-14 09:23:51 +0900

    localizatoin stuff

---
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,35 @@
+﻿ using System.Collections.Generic;
+ using System.Collections;
+ using System.IO;
+ using UnityEngine;
+
+ public class LocalizationManager : MonoBehaviour {
+
+     private Dictionary<string, string> localizedText;
+
+     void Start () {
+
+     }
+
+     public void LoadLocalizedText (string fileName)
+      {
+         localizedText = new Dictionary<string, string> ();
+         string filePath = Path.Combine (Application.streamingAssetsPath, fileName);
+
+         if (File.Exists (filePath)) {
+             string dataAsJson = File.ReadAllText (filePath);
+             LocalizationData loadedData = JsonUtility.FromJson<LocalizationData> (dataAsJson);
+
+             for (int i = 0; i < loadedData.items.Length; i++) {
+                 localizedText.Add (loadedData.items[i].key, loadedData.items[i].value);
+             }
+
+             Debug.Log ("Data loaded, dictionary contains: " + localizedText.Count + "entries");
+
+         } else {
+             Debug.LogError ("Cannot find file!");
+         }
+
+     }
+
+ }
